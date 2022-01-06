122cc243f6e414aaad1672d16ac66cc3869f47d5
tochika11 <tochika11@gmail.com>
2022-01-06 23:47:44 +0900

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Tochika! This is Aws Continuous deliver pipeline.'));
+app.get('/', (req, res) => res.send('Hi Tochika! This is Aws Continuous deliver pipeline. (Reviewed)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
